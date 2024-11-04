6c7b3cf57a0aa902771a1bbe3621d050bb4f1f95
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-11-04 12:09:10 +0200

    Pygame screen has been added

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import pygame
-from constants import *
+import constants
 
 def main():
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     GAME_ON = True
     while GAME_ON:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill("#000000")
-        screen.update()
+        pygame.display.flip()
+
+
 if __name__ == "__main__":
     main()
