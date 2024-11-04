0922096f64a361be00702dee1662396a91e878e2
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-11-04 12:00:59 +0200

    main.py and constants.py set up

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import pygame
+from constants import *
 
 def main():
     print("Starting asteroids!")
-
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    GAME_ON = True
+    while GAME_ON:
+        screen.fill("#000000")
+        screen.update()
 if __name__ == "__main__":
     main()
