9bdcfbc240140f61844e740c52df147a52d76a45
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-11-05 13:49:24 +0200

    Added a move function to enable player forward and backward movement

--- main.py
+++ main.py
@@ -17,9 +17,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("#000000")
+        dt = game_clock.tick(60)/1000
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
-        dt = game_clock.tick(60)/1000
 
 
 if __name__ == "__main__":
