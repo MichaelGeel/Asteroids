464e4481dfe1521644030c23c9b54d998dd5a4b5
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-11-05 12:38:52 +0200

    Created a game clock to limit the FPS of the game

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     GAME_ON = True
+    game_clock = pygame.time.Clock()
+    dt = 0
     while GAME_ON:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("#000000")
         pygame.display.flip()
+        dt = game_clock.tick(60)/1000
+        print(dt)
 
 
 if __name__ == "__main__":
