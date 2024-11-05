ebff009ca9708d65e3b04130f01a565b0f8cccd9
MichaelGeel <mnrmichaelgeel@gmail.com>
2024-11-05 13:24:39 +0200

    Added the player character class

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import pygame
-import constants
+from constants import *
+from player import Player
 
 def main():
     print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     GAME_ON = True
     game_clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while GAME_ON:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("#000000")
+        player.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60)/1000
-        print(dt)
 
 
 if __name__ == "__main__":
